2f96c02446d0dfd3c05e2956facaa2af4b28bcb8
Josh DiGiovanni <jmdigiov@usc.edu>
2013-10-09 17:34:53 -0700

    Menu now includes prices

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -4,15 +4,28 @@ import java.util.*;
 
 public class Menu {
 	private List<String> items = new ArrayList<String>();
+	private Map<String, Integer> prices = new HashMap<String, Integer>();
 	
 	Menu() {}
 	
-	public void addItem(String item) {
+	public void addItem(String item, int price) {
 		items.add(item);
+		prices.put(item, price);
 	}
 	
 	public void removeItem(String item) {
 		items.remove(item);
+		prices.remove(item);
+	}
+	
+	public int lowestPrice() {
+		int low = 10000;
+		for (Integer price : prices.values()) {
+			if (price < low) {
+				low = price;
+			}
+		}
+		return low;
 	}
 	
 	public boolean checkItem(String item) {
